d151ca59970aa015f21435098dbb1f4897c183b4
arbeiter <awsidc@gmail.com>
2015-12-16 00:22:02 -0500

    Add a simple list to the main page with a static source

--- app/src/main/java/com/nomnommer/arbeiter/nomnommer/MainActivityFragment.java
+++ app/src/main/java/com/nomnommer/arbeiter/nomnommer/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,14 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayList<String> outputList = new ArrayList<String>();
+        outputList = new ArrayList<String>(Arrays.asList("Qdoba", "McDonalds", "Burga King"));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.fetched_item_display, R.id.fetched_item_text_view, outputList);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView)rootView.findViewById(R.id.list_view_fetchedItems);
+        listView.setAdapter(arrayAdapter);
+
+        return rootView;
     }
 }
